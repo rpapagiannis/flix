3b963962a1b9e68287c004a010d4443066f1eed9
RPapagiannis <rpapagiannis@icloud.com>
2024-10-16 07:39:15 +0300

    Adds methods to Movie and movies_helper

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,2 +1,9 @@
 module MoviesHelper
+  def total_gross(movie)
+    movie.flop? ? "Flop" : number_to_currency(movie.total_gross, precision: 0)
+  end
+
+  def year_of(movie)
+    movie.released_on.year
+  end
 end
