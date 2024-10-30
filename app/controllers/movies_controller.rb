ea920178d8b04ededbcf1833e2722c6175946609
RPapagiannis <rpapagiannis@icloud.com>
2024-10-30 20:54:37 +0200

    Adds flash messages to movie creation, update, and destruction

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -15,7 +15,7 @@ class MoviesController < ApplicationController
     @movie = Movie.find(params[:id])
 
     if @movie.update(movie_params)
-      redirect_to @movie
+      redirect_to @movie, notice: "Movie successfully updated"
     else
       render :edit, status: :unprocessable_entity
     end
@@ -28,7 +28,7 @@ class MoviesController < ApplicationController
   def create
     @movie = Movie.new(movie_params)
     if @movie.save
-      redirect_to @movie
+      redirect_to @movie, notice: "Movie successfully created"
     else
       render :new, status: :unprocessable_entity
     end
@@ -39,7 +39,7 @@ class MoviesController < ApplicationController
 
     @movie.destroy
 
-    redirect_to movies_url, status: :see_other
+    redirect_to movies_url, status: :see_other, alert: "Movie successfully deleted"
   end
 
   private
