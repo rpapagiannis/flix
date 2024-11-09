11701142b4a6287313aa2736f622f862ef047c51
RPapagiannis <rpapagiannis@icloud.com>
2024-11-09 16:15:04 +0200

    Improves email validation pattern

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :name, presence: true
-  validates :email, format: { with: /\S+@\S+/ }, uniqueness: { case_sensitive: false }
+  validates :email, format: { with: /\A\S+@\S+\.\S+\z/ }, uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 10, allow_blank: true }
 end
