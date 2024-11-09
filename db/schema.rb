249c3088c184c512c0aa5aaa1cdf1dc508c6a69a
RPapagiannis <rpapagiannis@icloud.com>
2024-11-09 15:45:58 +0200

    Upgrades to Rails 8

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_08_205854) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_09_132953) do
   create_table "movies", force: :cascade do |t|
     t.string "title"
     t.string "rating"
