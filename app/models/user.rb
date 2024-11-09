779b119fc84b1857fa78a13b8c7ac03c64208e2a
RPapagiannis <rpapagiannis@icloud.com>
2024-11-09 16:12:48 +0200

    Adds user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
+
+  validates :name, presence: true
+  validates :email, format: { with: /\S+@\S+/ }, uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 10, allow_blank: true }
 end
