a11b36ba15365625b4b8cb7da7b821699e96f2b1
RPapagiannis <rpapagiannis@icloud.com>
2024-11-18 21:31:30 +0200

    Add authorization to the user edit, updated, and destroy actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,21 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  def require_signin
+    unless current_user
+      session[:intended_url] = request.url
+      redirect_to signin_path, alert: "You need to sign in first!"
+    end
+  end
+
+  def current_user?(user)
+    current_user == user
+  end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  helper_method :current_user, :current_user?
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :require_signin, except: [ :new, :create ]
+  before_action :require_correct_user, only: [ :edit, :update, :destroy ]
+
   def index
     @users = User.all
   end
@@ -22,11 +25,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       redirect_to(@user, notice: "Account successfully updated")
     else
@@ -35,8 +36,6 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    @user = User.find(params[:id])
-
     @user.destroy
 
     session[:user_id] = nil
@@ -48,5 +47,12 @@ class UsersController < ApplicationController
   private
     def user_params
       params.require(:user).permit(:name, :username, :email, :password, :password_confirmation)
+    end
+
+    def require_correct_user
+      @user = User.find(params[:id])
+      unless current_user?(@user)
+        redirect_to movies_path, status: :see_other, alert: "Unauthorized access"
+      end
     end
 end
