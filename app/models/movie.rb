543acfe1288bb5d875ed61809659eb777ef64691
RPapagiannis <rpapagiannis@icloud.com>
2024-11-23 12:05:31 +0200

    Introduce favorite resource and associate it with movie and user"

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,6 +2,7 @@ class Movie < ApplicationRecord
   RATINGS = %w[G PG PG-13 R NC-17]
 
   has_many :reviews, dependent: :destroy
+  has_many :favorites, dependent: :destroy
 
   validates :title, :released_on, :duration, presence: true
   validates :description, length: { minimum: 30 }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_11_21_165815) do
+ActiveRecord::Schema[8.0].define(version: 2024_11_23_093855) do
+  create_table "favorites", force: :cascade do |t|
+    t.integer "movie_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["movie_id"], name: "index_favorites_on_movie_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
+
   create_table "movies", force: :cascade do |t|
     t.string "title"
     t.string "rating"
@@ -44,5 +53,7 @@ ActiveRecord::Schema[8.0].define(version: 2024_11_21_165815) do
     t.boolean "admin", default: false
   end
 
+  add_foreign_key "favorites", "movies"
+  add_foreign_key "favorites", "users"
   add_foreign_key "reviews", "movies"
 end
