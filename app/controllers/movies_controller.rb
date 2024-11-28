5b3db99010fa5d71b57ca68bb7109a18b628ca41
RPapagiannis <rpapagiannis@icloud.com>
2024-11-28 20:55:27 +0200

    List genres on movie show and add genre checkboxes to movie form partial

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,6 +8,7 @@ class MoviesController < ApplicationController
   def show
     @movie = Movie.find(params[:id])
     @fans = @movie.fans
+    @genres = @movie.genres.order(:name)
 
     if current_user
       @favorite = current_user.favorites.find_by(movie_id: @movie.id)
@@ -52,6 +53,6 @@ class MoviesController < ApplicationController
   private
     def movie_params
       params.require(:movie).permit(:title, :description, :rating, :released_on, :total_gross, :director, :duration,
-      :image_file_name)
+      :image_file_name, genre_ids: [])
     end
 end
